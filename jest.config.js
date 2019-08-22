5c1db1e14edec0efaa08fe7e27158573647d28e3
tqma <tqma@ctrip.com>
2019-08-22 16:41:45 +0800

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,34 @@
 module.exports = {
-  "globals": {
+  globals: {
     'ts-jest': {
-      "tsConfigFile": 'tsconfig.json'
-    }
+      tsConfig: 'tsconfig.json',
+    },
   },
-  "transform": {
-    ".(ts|tsx)": "ts-jest"
+  transform: {
+    '.(ts|tsx)': 'ts-jest',
   },
-  "testEnvironment": "jsdom",
-  "testRegex": "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js"
+  testEnvironment: 'jsdom',
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  moduleFileExtensions: [
+    'js',
+    'ts',
+    'tsx',
   ],
-  "coveragePathIgnorePatterns": [
-    "/node_modules/",
-    "/test/"
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/test/',
   ],
-  "coverageThreshold": {
-    "global": {
-      "branches": 90,
-      "functions": 95,
-      "lines": 95,
-      "statements": 95
-    }
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
   },
-  "collectCoverageFrom": [
-    "src/*.{js,ts}"
+  collectCoverageFrom: [
+    'src/*.{js,ts}',
   ],
-  "testEnvironment": "jsdom"
+  preset: 'ts-jest',
+  testMatch: null,
 }
